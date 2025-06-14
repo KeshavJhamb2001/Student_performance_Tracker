ad1120226d80def0bdc783790fd35d75ccd81793
keshav <keshavjhamb001@gmail.com>
2025-06-14 14:31:37 +0530

    commit

---
+++ src/main/java/com/keshav/student_performance_tracker/StudentPerformanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.keshav.student_performance_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPerformanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPerformanceTrackerApplication.class, args);
+    }
+
+}
